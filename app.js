15f532eaeb7567397b301bcecbb71e263784ecd2
phoenixyork166 <berlinlee2017@gmail.com>
2024-05-29 14:22:50 +0800

    pure node.js

--- app.js
+++ app.js
@@ -1,102 +1,42 @@
 // import a global module named http
-const fs = require('fs');
 const http = require('http');
-const path = require('path');
-const { format } = require('date-fns');
 
-// using ES6 arrow function
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    const method = req.method;
+const express = require('express');
 
-    if (url === '/') {
-        console.log(req.url, req.method, req.headers); // Log the request URL to the console
-        res.statusCode = 200; // HTTP res status code
-        // passing a header to res
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
-        res.write('<head><title>My Page</title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-        res.write('</html>');
+// creating an Express application
+const app = express();
 
-        // returning to stop subsequent code execution
-        return res.end();
-    }
+// Using Express Middleware
+app.use((req, res, next) => {
+    // This allows us to hook into this Funnel 
+    // through which the HTTP request to send
+    console.log(`In the middleware!`);
 
-    if (url === '/message' && method === 'POST') {
-        // preparing to store data chunk in an array
-        const body = [];
-
-        // Record dateTime for Message-${now}.txt
-        const now = new Date();
-        const formattedNow = format(now, 'yyyy-MM-dd__HH_mm_ss');
-
-        // Registering 'data' Event Listener to the Event Loop
-        // Listen on Data events using anonymous ES6 arrow function
-        req.on('data', (chunk) => {
-            // appending each data chunk
-            body.push(chunk);
-        });
-
-        // Registering 'end' Event Listener to the Event Loop
-        // fs.writeFile() will execute even Response is already gone
-        req.on('end', () => {
-            // Data is received in chunks (Instances of Buffer)
-            // Chunks are collected into an array
-            // Once all chunks are received (end Event Loop)
-            // they are concatenated using Buffer.concat()
-            // Need to know incoming data to be String
-            // Will need to change data type if parsing a File
-            const parsedBody = Buffer.concat(body).toString();
-            console.log(`Parsed Body:\n${parsedBody}`);
-
-            // message = spliting message=input
-            // [1] taking 2nd element => input
-            // [0] taking 1st element => message
-            const message = parsedBody.split('=')[1];
-            const messagePath = path.join(__dirname, 'message', `message-${formattedNow}.txt`);
-
-            try {
-                // async fs writeFile method
-                fs.writeFile(messagePath, message, (err) => {
-                    if (err) {
-                        console.error(`Error:\n${err}`);
-                        res.statusCode = 501;
-                        return res.end(`Server Internal Error`);
-                    }
-                    // HTTP status code for redirection
-                    res.statusCode = 302;
-                    // redirect to /
-                    res.setHeader('Location', '/');
-                    // return to exit a function
-                    return res.end(`Redirected to /`);
-            });
-            } catch (err) {
-                console.log(`Failed to execute Async file I/O:\n${err}`);
-            }
-        });
-        /*
-        req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-            fs.writeFileSync('message.txt', message);
-        });
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
-        */
-    } else {
-        res.statusCode = 404;
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
-        res.write('<head><title>Not Found</title></head>');
-        res.write('<body><h1>Page Not Found</h1></body>');
-        res.write('</html>');
-        res.end(`Page Not Found`);
-    }
+    // Adding next() to allow this Middleware
+    // to travel to the next Middleware
+    next();
 });
 
+// Adding another Express Middleware
+app.use((req, res, next) => {
+    // This allows us to hook into this Funnel 
+    // through which the HTTP request to send
+    console.log(`In another middleware!`);
+
+    // Overwritting this Express Middleware default Header
+    // res.setHeader();
+
+    // Send a HTTP response with a Body
+    res.status(200).send({ message: `Request received` });
+});
+
+// execute functions stored in ./routes
+// for incoming requests
+// const server = http.createServer(app);
+
+// console.log(`routes.someText: ${routes.someText}`);
+
 const port = 3005;
-server.listen(port, () => {
+app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
